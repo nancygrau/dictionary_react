8bd02adb3831a4199de1b79f4ccf2981abafbbeb
Nancy Grau <100781274+nancygrau@users.noreply.github.com>
2022-08-04 23:04:37 +0200

    Search engine form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import Dictionary from "./Dictionary.js"
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+      <header className="App-header">Dictionary
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Nancy Grau</small></footer>
+      </div>
     </div>
   );
 }
