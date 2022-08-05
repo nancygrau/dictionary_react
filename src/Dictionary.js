c55cf7dadba34a0ee340ffe84803fe3e4d668e36
Nancy Grau <100781274+nancygrau@users.noreply.github.com>
2022-08-05 11:44:27 +0200

    added some documentation for reference

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,6 +10,9 @@ function handleResponse(response){console.log(response.data[0]);}
 function search(event){
   event.preventDefault();
 
+
+  //documentation: https://dictionaryapi.dev/
+  
   let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(apiUrl).then(handleResponse);
 }
