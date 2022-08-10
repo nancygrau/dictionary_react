6c6a36aa046ef130d79006754a924ce045f6d13e
Nancy Grau <100781274+nancygrau@users.noreply.github.com>
2022-08-10 13:11:15 +0200

    Incorperated phonetics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,6 @@ let [keyword, setKeyword] = useState("");
 let[results, setResults]= useState(null);
 
 function handleResponse(response){
-  console.log(response.data[0]);
   setResults(response.data[0]);
   
 }
