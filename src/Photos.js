5c20012ebcc41e803f582f7af0383873c16536a5
Nancy Grau <100781274+nancygrau@users.noreply.github.com>
2022-08-11 21:41:34 +0200

    update photo code

--- src/Photos.js
+++ src/Photos.js
@@ -1,20 +1,20 @@
 import React from "react";
-import "./Photos.css"
+import "./Photos.css";
 
-export default function Photos (props) {
-  if (props.photos){
+export default function Photos(props) {
+  if (props.photos) {
     return (
       <section className="Photos">
         <div className="row">
-          {props.photos.map(function(photo, index){
-          return (
-            <div className="col-4" key={index}>
-              <a href={photo.src.original} target="_blank" rel="noreferrer">    
-                <img src={photo.src.landscape} className="img-fluid"/>
-              </a> 
-          </div>
-          ); 
-        })}
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
         </div>
       </section>
     );
